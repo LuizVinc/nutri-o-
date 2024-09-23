4b48b17f37b9f0f62024fe3802d4def282e0c67f
LuizVinc <165902281+LuizVinc@users.noreply.github.com>
2024-09-23 10:40:56 -0300

    23/09/2024

---
+++ main.js
@@ -0,0 +1,10 @@
+
+var titulo = document.querySelector("h1");
+titulo.textContent = "Lucas Nutrição";
+var paciente = document.querySelector("primeiro paciente"):
+var tdPeso = paciente.querySelector(".info-peso");
+tdPeso.textContent = "80";
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura = textContent;
